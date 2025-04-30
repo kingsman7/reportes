490bccf2de71c701d3ef1d7b84810e0fc2a30926
Angel Barrios <ae.barrios@hotmail.com>
2025-04-30 09:10:40 -0400

    add redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { ChartHomicidesComponent } from "./components/homicides/components/chart-homicides/chart-homicides.component";
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    DashboardComponent,
+    RouterOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
