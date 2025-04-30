c045ee45a6275ba046a84fdc52b8f44cee7113e3
Angel Barrios <ae.barrios@hotmail.com>
2025-04-30 08:10:55 -0400

    dahsboardComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
+    DashboardComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
